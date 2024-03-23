bb05a7c35c7cb2bd7aa9eeba4a49ad6fc2e5030f
Thxnxt <141336398+Thxnxt@users.noreply.github.com>
2024-03-24 02:17:07 +0700

    shortๆ

---
+++ src/Register.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Register {
+    private JFrame fr;
+    private JPanel pn;
+    private JTextField fx;
+
+    public Register() {
+        fr = new JFrame();
+        
+    }
+}
