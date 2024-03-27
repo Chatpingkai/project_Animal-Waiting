426f6a9a44900b20aa0111bd3917a1aa746b5acd
Thxnxt <141336398+Thxnxt@users.noreply.github.com>
2024-03-28 04:05:31 +0700

    งง

--- src/Main_user.java
+++ src/Main_user.java
@@ -7,16 +7,20 @@ import java.awt.image.BufferedImage;
 public class Main_user implements ActionListener {
 
     private JFrame fr;
-    private JPanel pn, main, line1, line2, line3, colorL, colorR, ex1, buttonPanel, bordercalen, empty;
-    private JLabel info, imagelogoLabel;
+    private JPanel info, pn, main, line1, line2, line3, colorL, colorR, ex1, buttonPanel, bordercalen, empty, namePanel, petnamePanel,
+     typePanel, breedPanel, datePanel, sexPanel, agePanel, weightPanel, callPanel, mailPanel, addressPanel, emptyline, something, 
+     infotop, infobottom, empty1, empty2, empty3, empty4;
+    private JLabel imagelogoLabel, nameLabel, petnameLabel, typeLabel, breedLabel, dateLabel, 
+        sexLabel, ageLabel, weightLabel, callLabel, mailLabel, addressLabel;
     private JButton edit, history, logout, reserve;
     private ImageIcon imagelogo, resizedImageIcon, roundedIcon;
 
     public Main_user() {
 
-        //ประกาศทั้งหมด
+        //JFrame
         fr = new JFrame("Main");
-        pn = new JPanel(new FlowLayout());
+        //JPanel
+        pn = new JPanel(new BorderLayout());
         main = new JPanel(new BorderLayout());
         line1 = new JPanel(new FlowLayout());
         line2 = new JPanel(new FlowLayout());
@@ -24,10 +28,42 @@ public class Main_user implements ActionListener {
         colorL = new JPanel(new BorderLayout());
         colorR = new JPanel(new FlowLayout());
         ex1 = new JPanel(new FlowLayout());
-        buttonPanel = new JPanel(new GridLayout(3, 1));
+        buttonPanel = new JPanel(new GridLayout(4, 1));
         bordercalen = new JPanel(new FlowLayout());
         empty = new JPanel(new FlowLayout());
-        info = new JLabel();
+        namePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        petnamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        typePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        breedPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        datePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        sexPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        agePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        weightPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        callPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        mailPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        addressPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        emptyline = new JPanel();
+        info = new JPanel(new BorderLayout());
+        something = new JPanel(new BorderLayout());
+        infotop = new JPanel(new GridLayout(10,1));
+        infobottom = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        empty1 = new JPanel();
+        empty2 = new JPanel();
+        empty3 = new JPanel();
+        empty4 = new JPanel();
+        //JLabel
+        nameLabel = new JLabel("ชื่อ-นามสกุล :");
+        petnameLabel = new JLabel("ชื่อสัตว์เลี้ยง :");
+        typeLabel = new JLabel("ชนิด :");
+        breedLabel = new JLabel("สายพันธ์ุ :");
+        dateLabel = new JLabel("วัน/เดือน/ปีเกิด :");
+        sexLabel = new JLabel("เพศ :");
+        ageLabel = new JLabel("อายุ :");
+        weightLabel = new JLabel("น้ำหนัก :");
+        callLabel = new JLabel("เบอร์โทรศัพท์ :");
+        mailLabel = new JLabel("E-mail :");
+        addressLabel = new JLabel("ที่อยู่ : ");
+        //JButton
         edit = new JButton("แก้ไขข้อมูล");
         history = new JButton("ประวัติการรักษา");
         logout = new JButton("Logout");
@@ -40,26 +76,29 @@ public class Main_user implements ActionListener {
         imagelogoLabel = new JLabel(roundedIcon);
         colorL.setBackground(new Color(0xFFE3A8));
         colorR.setBackground(new Color(255, 238, 227));
-        colorL.setPreferredSize(new Dimension(250, 1000));
+        colorL.setPreferredSize(new Dimension(350, 1000));
         ex1.setBackground(new Color(0xFEE3A8));
         ex1.add(imagelogoLabel);
 
-        info.setBackground(new Color(0xFDFAE5));
-        info.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        
 
         //button southwest
         edit.setBackground(Color.WHITE);
         edit.setFocusPainted(false);
         edit.setBorder(BorderFactory.createLineBorder(Color.black, 2));
-        edit.setSize(new Dimension());
         edit.setPreferredSize(new Dimension(120, 40));
 
         history.setBackground(Color.WHITE);
         history.setFocusPainted(false);
         history.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        history.setPreferredSize(new Dimension(120, 40));
+
         logout.setBackground(Color.WHITE);
         logout.setFocusPainted(false);
         logout.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        logout.setPreferredSize(new Dimension(120, 40));
+
+        emptyline.setBackground(new Color(0xFEE3A8));
         line1.add(edit);
         line2.add(history);
         line3.add(logout);
@@ -69,30 +108,76 @@ public class Main_user implements ActionListener {
         buttonPanel.add(line1);
         buttonPanel.add(line2);
         buttonPanel.add(line3);
+        buttonPanel.add(emptyline);
 
-
-
-
-
+        //Infomation
+        something.setBackground(new Color(0xFEE3A8));
+        something.add(info, BorderLayout.CENTER);
+        something.add(empty1, BorderLayout.NORTH);
+        something.add(empty2, BorderLayout.WEST);
+        something.add(empty3, BorderLayout.EAST);
+        something.add(empty4, BorderLayout.SOUTH);
+        empty1.setBackground(new Color(0xFEE3A8));
+        empty2.setBackground(new Color(0xFEE3A8));
+        empty3.setBackground(new Color(0xFEE3A8));
+        empty4.setBackground(new Color(0xFEE3A8));
+        info.setBackground(new Color(0xFDFAE5));
+        info.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        info.add(infotop, BorderLayout.CENTER);
+        info.add(infobottom, BorderLayout.SOUTH);
+        infobottom.setBackground(new Color(0xFDFAE5));
+        infobottom.add(addressPanel);
+        infotop.setBackground(new Color(0xFDFAE5));
+        namePanel.setBackground(new Color(0xFDFAE5));
+        petnamePanel.setBackground(new Color(0xFDFAE5));
+        typePanel.setBackground(new Color(0xFDFAE5));
+        breedPanel.setBackground(new Color(0xFDFAE5));
+        datePanel.setBackground(new Color(0xFDFAE5));
+        sexPanel.setBackground(new Color(0xFDFAE5));
+        agePanel.setBackground(new Color(0xFDFAE5));
+        weightPanel.setBackground(new Color(0xFDFAE5));
+        callPanel.setBackground(new Color(0xFDFAE5));
+        mailPanel.setBackground(new Color(0xFDFAE5));
+        addressPanel.setBackground(new Color(0xFDFAE5));
+        namePanel.add(nameLabel);
+        petnamePanel.add(petnameLabel);
+        typePanel.add(typeLabel);
+        breedPanel.add(breedLabel);
+        datePanel.add(dateLabel);
+        sexPanel.add(sexLabel);
+        agePanel.add(ageLabel);
+        weightPanel.add(weightLabel);
+        callPanel.add(callLabel);
+        mailPanel.add(mailLabel);
+        addressPanel.add(addressLabel);
+        infotop.add(namePanel);
+        infotop.add(petnamePanel);
+        infotop.add(typePanel);
+        infotop.add(breedPanel);
+        infotop.add(datePanel);
+        infotop.add(sexPanel);
+        infotop.add(agePanel);
+        infotop.add(weightPanel);
+        infotop.add(callPanel);
+        infotop.add(mailPanel);
 
         //Main
         bordercalen.setSize(100,1000);
         bordercalen.setBackground(new Color(0xFEE3A8));
-
 
         empty.add(bordercalen);
         empty.setBackground(new Color(0xFEEE3A8));
         empty.setBounds(200,220,1000,1000);
 
         //setting and addcomponent
-
-        pn.add(buttonPanel);
+        pn.add(something);
+        pn.add(buttonPanel, BorderLayout.SOUTH);
         pn.setBackground(new Color(0xFEE3A8));
 
         colorL.add(ex1, BorderLayout.NORTH);
         colorL.add(pn, BorderLayout.CENTER);
 
-        colorR.add(empty);
+        colorR.add(empty);//เดี๋ยวแก้
 
         main.setSize(1000, 850);
         main.add(colorL, BorderLayout.WEST);
@@ -101,7 +186,7 @@ public class Main_user implements ActionListener {
         fr.add(main);
         fr.setResizable(false);
         fr.setDefaultCloseOperation(fr.EXIT_ON_CLOSE);
-        fr.setSize(1000, 850);
+        fr.setSize(1200, 950);
         fr.setVisible(true);
 
         //AddListener
@@ -145,7 +230,7 @@ public class Main_user implements ActionListener {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        SwingUtilities.invokeLater(() -> new Main_user());
+        new Main_user();
     }
 
 }
