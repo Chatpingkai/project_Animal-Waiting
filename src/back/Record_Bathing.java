2f450c31dd739ddf0b40629214759ee433b0226d
ENdDoWn <folkza1212123@hotmail.com>
2024-03-28 05:02:21 +0700

    Create class structure
    some attribute i don't know type of it so i commented

---
+++ src/back/Record_Bathing.java
@@ -0,0 +1,16 @@
+
+package back;
+
+/**
+ *
+ * @author ENdDoWn
+ */
+public class Record_Bathing {
+    //private <type> date; <-- don't know type
+//    public <type> getDate() {
+//        return this.date;
+//    }
+//    public void setDate(<type> date) {
+//        this.date = date;
+//    }
+}
---
+++ src/back/Use_Med.java
@@ -0,0 +1,19 @@
+
+package back;
+
+/**
+ *
+ * @author ENdDoWn
+ */
+
+public class Use_Med {
+    private Med usemed;
+    
+    public Med getUsemed() {
+        return usemed;
+    }
+    
+    public void setUsemed(Med set) {
+        this.usemed = set;
+    }
+}
